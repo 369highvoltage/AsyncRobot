b08fb6fddec18c341d910c71fa9cdbf77f40b64d
Eric <Treayn@gmail.com>
2018-06-04 20:51:46 -0400

    Clean up

--- Command.py
+++ Command.py
@@ -7,21 +7,54 @@ class Command():
 
     def __init__(self, persistent=False: bool):
         self.components = []
+        self.elapsed_time = 0.0
         self.interrupt_event = None
         self.persistent = persistent
         self.task = None
     
-    # Private Methods
-    def _acquire_resources(self) -> bool:
+    
+    # Coroutines
+    async def _run(self):
+        """Coroutine run on the event loop"""
+        
+        self._initialize()
 
-        # Attempt to lock all subsystems.
-        if any([component.locked() for component in self.components])
-            return False
-        else:
-            for component in self.components:
-                component.acquire()
+        while True:
+            if self.isFinished():
+                self._end()
+                break
+            elif self.interrupt_event.is_set():
+                self.interrupted()
+                self._release_resources()
+
+                if self.persistent:
+                    await self._suspend()
+                else:
+                    self._terminate()
+                    break
             
-            return True
+            self._execute()
+            await asyncio.sleep(Command.DEFAULT_PERIOD - self.elapsed_time)
+        
+        return
+    
+    async def _suspend(self):
+        await asyncio.sleep(0)
+        while self._check_resources():
+            await asyncio.wait([component.wait() for component in self.components])
+        
+        self._acquire_resources()
+        self.interrupt_event.clear()
+        return
+
+
+    # Private Methods
+    def _check_resources(self) -> bool:
+        return any([component.locked() for component in self.components])
+
+    def _acquire_resources(self) -> None:
+        for component in self.components:
+            component.acquire()
 
     def _release_resources(self):
         for component in self.components:
@@ -29,42 +62,15 @@ class Command():
 
             # Notify in case some Teleop commands were suspended.
             component.notify()
-
-    async def _run(self):
-        """Coroutine run on the event loop"""
-        
-        # Check & lock all components else abort.
-        if not self._acquire_resources():
-            return
-        
-        self.initialize()
-
-        while not self.isFinished():
-            if self.interrupt_event.is_set():
-                self.interrupted()
-
-                if self.persistent:
-                    self._release_resources()
-                    await asyncio.wait([component.wait() for component in self.components])
-                    self._acquire_resources()
-                    self.interrupt_event.clear()
-                else:
-                    self.task.cancel()
-            
-            # Keep a constant interval.
-            interval_start = datetime.now()
-            self.execute()
-            interval_elapsed = min(Command.DEFAULT_PERIOD, (datetime.now() - start).seconds)
-            await asyncio.sleep(Command.DEFAULT_PERIOD - interval_elapsed)
-        
-        self.end()
-        self._release_resources()
-        return
     
     def _set_interrupt(self, event) -> None:
         """Assign global interrupt from CommandGroup to Command"""
         self.interrupt_event = event
+    
+    def _terminate(self):
+        self.task.cancel()
 
+    
     # Public Methods
     def cancel(self):
         """Called by other Commands or CommandGroups"""
@@ -84,6 +90,24 @@ class Command():
             asyncio.get_event_loop().run_until_complete(self.task)
         except asyncio.CancelledError:
             print("INFO: Command "+ self.__class__.__name__ +" interrupted.")
+    
+    def _end(self):
+        self.end()
+        self._release_resources()
+
+    def _execute(self):
+        # Keep a constant interval.
+        start_time = datetime.now()
+        self.execute()
+        self.elapsed_time = min(Command.DEFAULT_PERIOD, (datetime.now() - start).seconds)
+    
+    def _initialize(self):
+        if self._check_resources():
+            print('Subsystem in use by another command')
+        else:
+            self._acquire_resources()
+            self.initialize()
+            
 
     # User-Defined Methods
     def end(self) -> None:
